dcd0814e220114ed5acc5c82608775cd850bfb4f
Egor Denisov <deneg99@mail.ru>
2023-02-20 00:01:41 +0300

    Created simple scene with light

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three'
 import { FlyControls } from "three/examples/jsm/controls/FlyControls"
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 
 let clock = new THREE.Clock();
@@ -20,7 +21,13 @@ sceneElm.appendChild( renderer.domElement );
 const texture = new THREE.TextureLoader().load( 'https://raw.githubusercontent.com/mrdoob/three.js/master/examples/textures/crate.gif' );
 
 const geometry = new THREE.BoxGeometry( 200, 200, 200 );
-const material = new THREE.MeshBasicMaterial( { map: texture} );
+const material = new THREE.MeshPhysicalMaterial({
+	color: 0x2f2f2f,
+	thickness: 2,
+	metalness: 1,
+	reflectivity: 0,
+	transparent: 0,
+});
 let cube1 = new THREE.Mesh( geometry, material );
 cube1.rotation.x = 0;
 cube1.rotation.y = 0;
@@ -39,21 +46,55 @@ cube2.position.y = -150;
 cube2.position.z = geometry.parameters.height / 2;
 scene.add( cube2 );
 
+const geometry2 = new THREE.BoxGeometry( 100, 100, 100 );
+let cube3 = new THREE.Mesh( geometry2, material );
+cube3.rotation.x = 0;
+cube3.rotation.y = 0;
+cube3.rotation.z = 0;
+cube3.position.x = 150;
+cube3.position.y = -50;
+cube3.position.z = geometry2.parameters.height / 2;
+scene.add( cube3 );
+
 // const axesHelper = new THREE.AxesHelper( 500 );
 // scene.add( axesHelper );
 
 // const texture_earth = new THREE.TextureLoader().load( 'https://raw.githubusercontent.com/mrdoob/three.js/master/examples/textures/minecraft/grass.png' );
-const texture_earth = new THREE.TextureLoader().load( "./neon.jpg" );
+const texture_earth = new THREE.TextureLoader().load( "https://raw.githubusercontent.com/egordenisov/js-site/dev/textures/grass.jpg" );
 texture_earth.magFilter = THREE.NearestFilter;
-texture_earth.anisotropy = 32
-texture_earth.repeat.set(100, 100)
-texture_earth.wrapT = THREE.RepeatWrapping
-texture_earth.wrapS = THREE.RepeatWrapping
-const material_earth = new THREE.MeshBasicMaterial( {map: texture_earth} );
+texture_earth.anisotropy = 1;
+texture_earth.repeat.set(100, 100);
+texture_earth.wrapT = THREE.RepeatWrapping;
+texture_earth.wrapS = THREE.RepeatWrapping;
+const material_earth = new THREE.MeshPhysicalMaterial( {color: "white"} );
 let geom_plane = new THREE.PlaneBufferGeometry(10000, 10000)
-let plane1 = new THREE.Mesh( geom_plane, material_earth );
+let plane1 = new THREE.Mesh( geom_plane, material );
 scene.add( plane1 );
 
+const light = new THREE.PointLight( 0x0000ff, 3 );
+light.position.set( -150, 150, 50 );
+light.visible = true;
+scene.add( light );
+
+const light_amb = new THREE.AmbientLight( 0xffffff , 5); // soft white light
+light_amb.position.set(0 ,0 , 300);
+scene.add( light_amb );
+
+const met_tetr = new THREE.MeshPhysicalMaterial({
+	color: 0xffffff,
+	roughness: 0,  
+	transmission: 1,  
+	thickness: 0.5, // Add refraction!
+});
+const geom_tetr = new THREE.TetrahedronGeometry(100);
+let tetr1 = new THREE.Mesh( geom_tetr, met_tetr );
+tetr1.position.x = 200;
+tetr1.position.y = -50;
+tetr1.position.z = 50;
+tetr1.rotation.x = Math.PI*(5/6);
+tetr1.rotation.y = 0;
+tetr1.rotation.z = Math.PI*(5/6);
+// scene.add( tetr1 );
 
 
 camera.position.set( 400, 0, 100 );
@@ -70,7 +111,42 @@ controls.rollSpeed = Math.PI / 4;
 controls.autoForward = false;
 controls.dragToLook = true;
 
+const g_vs = new THREE.BoxGeometry( 700, 20, 200 );
+let cube4 = new THREE.Mesh( g_vs, material );
+cube4.rotation.set(0, 0, 0);
+cube4.position.set(500, 500, (g_vs.parameters.depth / 2));
+scene.add( cube4 );
 
+let cube5 = new THREE.Mesh( g_vs, material );
+cube5.rotation.set(0, 0, 0);
+cube5.position.set(500, 750, g_vs.parameters.depth / 2);
+scene.add( cube5 );
+
+const g_h = new THREE.BoxGeometry( 700, 270, 20 );
+let cube6 = new THREE.Mesh( g_h, material );
+cube6.rotation.set(0, 0, 0);
+cube6.position.set(500, 625, 210);
+scene.add( cube6 );
+
+const g_vr = new THREE.BoxGeometry( 20, 270, 220 );
+let cube7 = new THREE.Mesh( g_vr, material );
+cube7.rotation.set(0, 0, 0);
+cube7.position.set(860, 625, g_vr.parameters.depth / 2);
+scene.add( cube7 );
+
+const light_g = new THREE.PointLight( 0xff0f0f, 3 );
+light_g.position.set( 700, 625, 150 );
+light_g.visible = true;
+scene.add( light_g );
+
+
+// const loader = new GLTFLoader();
+
+// loader.load( 'https://github.com/egordenisov/js-site/blob/dev/3d/tesla/source/Cyber.glb?raw=true', function ( gltf ) {
+// 	scene.add( gltf.scene );
+// }, undefined, function ( error ) {
+// 	console.error( error );
+// });
 
 // const controls = new ArcballControls( camera, renderer.domElement, scene );
 
@@ -108,18 +184,19 @@ function animate() {
 	// if (camera.position.z > 1000 ){
 	// 	camera.position.z = 1000;
 	// }
+	const xy_restricts = 1200;
 	camera.position.z = 100;
-	if ( camera.position.x < -1000 ){
-		camera.position.x = -1000;
+	if ( camera.position.x < -xy_restricts ){
+		camera.position.x = -xy_restricts;
 	}
-	if (camera.position.x > 1000 ){
-		camera.position.x = 1000;
+	if (camera.position.x > xy_restricts ){
+		camera.position.x = xy_restricts;
 	}
-	if ( camera.position.y < -1000 ){
-		camera.position.y = -1000;
+	if ( camera.position.y < -xy_restricts ){
+		camera.position.y = -xy_restricts;
 	}
-	if (camera.position.y > 1000 ){
-		camera.position.y = 1000;
+	if (camera.position.y > xy_restricts ){
+		camera.position.y = xy_restricts;
 	}
 	renderer.render( scene, camera );
 };
